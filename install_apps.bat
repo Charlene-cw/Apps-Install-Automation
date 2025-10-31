26f6b9f8f1fe225417f1ea335b0108ecbc573662
Charlene Wong <119715392+Charlene-cw@users.noreply.github.com>
2025-10-31 13:06:04 +0800

    Add files via upload

---
+++ install_apps.bat
@@ -0,0 +1,30 @@
+@echo off
+echo Starting app installations...
+
+cd /d "%~dp0Apps_installers"
+
+:: EXE installers (silent flags vary by app)
+start /wait "" "7z2409-x64.exe" /S
+start /wait "" "AcroRdrDC2500120577_en_US.exe" /sAll
+start /wait "" "ChromeSetup.exe" /silent /install
+start /wait "" "Firefox-Installer.exe" /silent /install
+start /wait "" "Linkus-lite-win-setup.exe" /S
+start /wait "" "MSTeamsSetup.exe" /quiet
+start /wait "" "Office365EntSetup.exe" /configure config.xml
+start /wait "" "ZoomInstallerFull.exe" /quiet
+
+
+:: MSI installers
+msiexec /i "AnyDesk.msi" /qn /norestart
+msiexec /i "MomSmartClient_x64.msi_windows_x64_2024.3.2.3.msi" /qn /norestart
+msiexec /i "pdfsam-5.1.3.msi" /qn /norestart
+
+echo Installing VPN tools...
+
+:: Change directory to the VPN folder
+cd /d "%~dp0VPN"
+:: Install VPN
+start /wait "" "FortiClientVPN.exe" /quiet
+
+echo All installations completed.
+pause
